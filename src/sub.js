d059b1d7f241d2397a8624209bd9f0631e79cd20
runoneall <runoobsteve@gmail.com>
2026-06-16 17:49:23 +0800

    feat: add p-limit for concurrency control in GET endpoint and enhance error handling

--- src/sub.js
+++ src/sub.js
@@ -4,6 +4,29 @@ const sub = express.Router()
 import { getenv } from './cfenv.js'
 const env = getenv()
 
-sub.get('/get', async (req, res) => {})
+import pLimit from 'p-limit'
+const limit = pLimit(3)
+
+sub.get('/get', async (req, res) => {
+    const { secret } = req.query
+    if (secret !== env.sub_secret) {
+        return res.status(403).send('密钥错误')
+    }
+
+    const keys = (await env.data.list()).keys
+    const subNames = keys.map(item => item.name)
+
+    const values = await env.data.get(subNames)
+    const subs = Object.fromEntries(values)
+
+    const promises = Object.entries(subs).map(([key, value]) => {
+        return limit(async () => {
+            const obj = JSON.parse(value)
+        })
+    })
+
+    const result = (await Promise.all(promises)).flat()
+    return res.status(200).json(result)
+})
 
 export default sub
